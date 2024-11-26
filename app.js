fdb622e3d7f5afe26479259098252699dd37358b
shaiksha176 <iamshaiksha17@gmail.com>
2024-11-27 00:09:21 +0530

    first commit

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get("/", (req, res) => {
   res.send("Express is running");
 });
 
+app.get("/api", (req, res) => {
+  res.json({
+    message: "API is running",
+  });
+});
+
 app.listen(port, () => console.log("server running on port ", port));
